8f0c28dafc0f7d21e32d4975d38b8e08077836e9
wren \\ evvil.town <16328864+sharkked@users.noreply.github.com>
2024-07-23 00:53:11 -0500

    sunset

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,29 +22,55 @@
         }
     }
 
-    function handleTogglePlaying(event) {
+    function startPlaying() {
         if (!player) return;
-        event.target.innerHTML = "";
         let playButton = document.querySelector("#play-pause");
         playButton.innerHTML = "";
-        if (playing) {
-            new PlayIcon({
-                target: playButton,
-            });
-            player.stopVideo();
+        new PauseIcon({
+            target: playButton,
+        });
+        player.playVideo();
+        document.documentElement.setAttribute("data-playing", true);
+    }
+
+    function stopPlaying() {
+        if (!player) return;
+        let playButton = document.querySelector("#play-pause");
+        playButton.innerHTML = "";
+        new PlayIcon({
+            target: playButton,
+        });
+        player.stopVideo();
+        document.documentElement.removeAttribute("data-playing");
+    }
+
+    function handleScroll(event) {
+        let volumeSlider = document.querySelector("input[type='range']");
+        if (event.deltaY < 0) {
+            volumeSlider.valueAsNumber += 5;
         } else {
-            new PauseIcon({
-                target: playButton,
-            });
-            player.setVolume(volume);
-            player.playVideo();
+            volumeSlider.value -= 5;
+        }
+        volume = volumeSlider.value;
+        if (!player) return;
+        ytPlayerVolume = volume;
+        player.setVolume(ytPlayerVolume);
+        event.stopPropagation();
+    }
+
+    function handleTogglePlaying(event) {
+        if (!player) return;
+        if (playing) {
+            stopPlaying();
+        } else {
+            startPlaying();
         }
         playing = !playing;
     }
 
     function handlePlayerReady(event) {
-        event.target.playVideo();
         event.target.setVolume(volume);
+        startPlaying();
     }
 
     function handlePlayerStateChanged(event) {
@@ -64,6 +90,8 @@
                 },
             });
         };
+
+        window.onwheel = handleScroll;
     });
 </script>
 
@@ -71,8 +99,9 @@
     <script src="https://www.youtube.com/iframe_api"></script>
 </svelte:head>
 
-<div id="yt-player"></div>
 <main>
+    <div id="yt-player"></div>
+    <div id="app-background"></div>
     <button
         id="play-pause"
         class="media-controls-button"
@@ -88,16 +117,6 @@
             max="100"
             value="50"
             on:input={handleVolumeChanged}
-            on:wheel={(event) => {
-                if (event.deltaY < 0) {
-                    event.target.valueAsNumber += 5;
-                } else {
-                    event.target.value -= 5;
-                }
-                handleVolumeChanged(event);
-                event.preventDefault();
-                event.stopPropagation();
-            }}
             class="slider"
             id="volume-slider"
         />
@@ -117,6 +136,27 @@
         flex-direction: column;
         align-items: center;
         place-content: center;
+    }
+
+    #app-background {
+        display: block;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        pointer-events: none;
+        background: rgb(114, 81, 35);
+        background: radial-gradient(
+            circle at 50% 120% in hsl,
+            rgb(88, 52, 22) 0%,
+            rgb(82, 30, 26) 50%,
+            rgba(53, 18, 36, 1) 69%,
+            rgba(2, 0, 36, 1) 110%
+        );
+        z-index: -1;
+        transition: opacity 0.2s;
+        opacity: 0;
     }
 
     .media-controls-button {
@@ -166,7 +206,7 @@
     input[type="range"]::-webkit-slider-thumb {
         width: 10px;
         height: 15px;
-        background: white;
+        background: var(--c-foreground);
         box-shadow: -80px 0 0 80px var(--c-accent);
         cursor: ew-resize;
         -webkit-appearance: none;
